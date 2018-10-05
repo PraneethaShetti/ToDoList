09f984ba36fcfd0ec81f9c5502b69b12f924b706
PraneethaShetti <pshetti062@gmail.com>
2018-10-05 10:55:52 -0700

    Updates

---
+++ precache-manifest.b9a56d8eaf761d3752a7a216b1fd7729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "b6a629a45324478a89bf",
+    "url": "/todolist/static/js/main.b6a629a4.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/todolist/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "b6a629a45324478a89bf",
+    "url": "/todolist/static/css/main.1a19f4ee.chunk.css"
+  },
+  {
+    "revision": "b0fde16716fbf8f1496b412138a88d0a",
+    "url": "/todolist/index.html"
+  }
+];
